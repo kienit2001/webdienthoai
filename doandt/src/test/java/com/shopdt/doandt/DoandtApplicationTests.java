2800a8a0435d4fb1c532f7f7358e6e63620a0e38
kienitpro890 <trungkiencntt516@gmail.com>
2022-12-22 14:28:58 +0700

    begin

---
+++ doandt/src/test/java/com/shopdt/doandt/DoandtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopdt.doandt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoandtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
